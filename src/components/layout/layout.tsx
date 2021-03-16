5d0d54bb2fd1571ff0e95ca25c02829a176891d4
nazariyv <nazariy@inbox.ru>
2021-03-16 01:04:33 +0000

    query params

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -17,6 +17,22 @@ const Layout: React.FC = ({ children }) => {
           <div className="content-wrapper mb-l">
             <div className="header">
               <div className="header__logo"></div>
+              <div className="soc">
+                <a
+                  href="https://discord.gg/ka2u9n5sWs"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <span className="discord"></span>
+                </a>
+                <a
+                  href="https://twitter.com/renftlabs"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <span className="twitter"></span>
+                </a>
+              </div>
               <div className="header__user">{short(currentAddress)}</div>
             </div>
           </div>
