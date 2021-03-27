a92ed1e8dad60cf5012df0a4d026ce6ee0bff13a
Sundeep Charan Ramkumar <webdevdesign@sundeepcharan.com>
2021-03-27 06:11:34 +0530

    chore: migrate contexts

---
+++ contexts/graph/types.ts
@@ -0,0 +1,41 @@
+export type AdoptionRequest = {
+  id: string;
+  price: string;
+  from: string;
+  timestamp: string;
+  active: boolean;
+  filled: boolean;
+  rescueTimestamp: string;
+};
+
+export type AdoptionOffer = {
+  id: string;
+  price: string;
+  to: string;
+  timestamp: string;
+  active: boolean;
+  filled: boolean;
+  catRescueTimestamp: string;
+};
+
+export type Cat = {
+  id: string;
+  name?: string;
+  isWrapped: boolean;
+  rescueTimestamp: string;
+  activeRequest?: AdoptionRequest;
+  activeOffer?: AdoptionOffer;
+  provenance?: {
+    requestPrices?: AdoptionRequest[];
+    offerPrices?: AdoptionOffer[];
+  };
+};
+
+export type CatInfo = {
+  catId: string;
+  color: string;
+  palette: string;
+  pattern: number;
+  statisticalRank: string;
+  traitRarityRank: string;
+};
