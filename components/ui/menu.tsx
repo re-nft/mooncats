403127fc92510df6567c34854f555a7cf8d5fb74
Sundeep Charan Ramkumar <webdevdesign@sundeepcharan.com>
2021-03-28 19:15:44 +0530

    chore: add routes data structure for easy active route calculation

--- components/ui/menu.tsx
+++ components/ui/menu.tsx
@@ -1,24 +1,51 @@
 import NextLink from 'next/link';
+import { useRouter } from 'next/router';
+
+const routes = [
+  {
+    pathname: '/',
+    routeName: 'all-cats',
+    value: 'All Cats',
+  },
+  {
+    pathname: '/offered',
+    routeName: 'offered',
+    value: 'Offered',
+  },
+  {
+    pathname: '/my-cats',
+    routeName: 'my-cats',
+    value: 'My Cats',
+  },
+  {
+    pathname: '/my-requests',
+    routeName: 'my-requests',
+    value: 'My requests',
+  },
+  {
+    pathname: '/cat/default',
+    routeName: 'default-cat',
+    value: 'show cat by id',
+  },
+];
 
 const Menu: React.FC = () => {
+  const router = useRouter();
   return (
     <div className="content-wrapper mb-l">
       <div className="menu">
-        <NextLink href="/">
-          <a className="menu__item">All Cats</a>
-        </NextLink>
-        <NextLink href="/offered">
-          <a className="menu__item">Offered</a>
-        </NextLink>
-        <NextLink href="/my-cats">
-          <a className="menu__item">My cats</a>
-        </NextLink>
-        <NextLink href="/my-requests">
-          <a className="menu__item">My requests</a>
-        </NextLink>
-        <NextLink href="/cat/default">
-          <a className="menu__item">Show cat by id</a>
-        </NextLink>
+        {routes.map((r) => (
+          <NextLink key={r.routeName} href={r.pathname}>
+            <a
+              title={r.routeName}
+              className={`menu__item ${
+                r.pathname === router.pathname ? 'active' : ''
+              }`}
+            >
+              {r.value}
+            </a>
+          </NextLink>
+        ))}
       </div>
     </div>
   );
