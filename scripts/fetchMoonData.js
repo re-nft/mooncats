7c1433e462d9a52d87bccbf7569c48227e6a7501
Sundeep Charan Ramkumar <webdevdesign@sundeepcharan.com>
2021-03-28 21:20:58 +0530

    feat: added prebuild script to fetch mooncats catinfo

---
+++ scripts/fetchMoonData.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const axios = require('axios');
+const path = require('path');
+const { promises: fs } = require('fs');
+const DATA_PATH = path.resolve(__dirname, '../', 'public', 'data.json');
+
+(async function () {
+  try {
+    console.log('fetching new mooncats !!');
+    const { data: response } = await axios.get(
+      'https://rarity.studio/files/new_mooncats.csv'
+    );
+
+    const rows = response.split('\n');
+    const data = rows.slice(1).reduce((acc, r) => {
+      const [
+        row,
+        catId,
+        palette,
+        pattern,
+        color,
+        statisticalRank,
+        traitRarityRank,
+      ] = r
+        .trim()
+        .split(',')
+        .map((el) => el.trim().replace(/"/g, ''));
+      acc.push({
+        row,
+        catId,
+        palette,
+        pattern,
+        color,
+        statisticalRank,
+        traitRarityRank,
+      });
+      return acc;
+    }, []);
+
+    if (await fs.stat(DATA_PATH)) await fs.unlink(DATA_PATH);
+
+    await fs.writeFile(
+      DATA_PATH,
+      JSON.stringify({ created_at: new Date().toISOString(), data })
+    );
+
+    console.log('done :)');
+  } catch (err) {
+    await fs.writeFile(
+      DATA_PATH,
+      JSON.stringify({ created_at: new Date().toISOString(), data: [] })
+    );
+    return;
+  }
+})();
