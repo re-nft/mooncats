63f61f5172cff15ed83adcf309bf67c924226097
Sundeep Charan Ramkumar <webdevdesign@sundeepcharan.com>
2021-03-30 00:26:09 +0530

    bugfix: add zlib for node-canvas

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 SCRIPT_DIR=$(dirname $0)
+yum install libuuid-devel libmount-devel && cp /lib64/{libuuid,libmount,libblkid}.so.1 node_modules/canvas/build/Release/
 node $SCRIPT_DIR/fetchMoonData.js  
