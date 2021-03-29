8204a24625c8b2c772659b1ded6c3122dea3bd6f
Sundeep Charan Ramkumar <webdevdesign@sundeepcharan.com>
2021-03-30 03:09:04 +0530

    fix: add cats folder command

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 SCRIPT_DIR=$(dirname $0)
 yum install libuuid-devel libmount-devel zlib-devel && cp /lib64/{libuuid,libmount,libblkid,libz}.so.1 node_modules/canvas/build/Release/
+mkdir -p /public/cats
 node $SCRIPT_DIR/fetchMoonData.js  
