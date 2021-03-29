9cd54807fbd41dfe3a698eb9d6fecfe53d8c0f94
Sundeep Charan Ramkumar <webdevdesign@sundeepcharan.com>
2021-03-30 01:02:58 +0530

    fix: zlib part 4

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env sh
 SCRIPT_DIR=$(dirname $0)
-yum install libuuid-devel libmount-devel zlib && cp /lib64/{libuuid,libmount,libblkid,libz}.so.1 node_modules/canvas/build/Release/
 node $SCRIPT_DIR/fetchMoonData.js  
