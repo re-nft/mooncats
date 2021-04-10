cb152b9af9173a3bd44be135c2ed0f53086ed708
Sundeep Charan Ramkumar <webdevdesign@sundeepcharan.com>
2021-04-10 13:03:55 +0530

    chore: codefactor fixes part 2

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 SCRIPT_DIR=$(dirname $0)
 yum install libuuid-devel libmount-devel zlib-devel && cp /lib64/{libuuid,libmount,libblkid,libz}.so.1 node_modules/canvas/build/Release/
 node $SCRIPT_DIR/fetchMoonData.js  
